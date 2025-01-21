3f2dfcd13af33e7df6fe54269fb2d9a0c97ab45c
supus19 <saaketh2127@gmail.com>
2025-01-21 09:20:10 -0800

    guess lab2

--- number_guessing_lab.py
+++ number_guessing_lab.py
@@ -1,5 +1,7 @@
-# this is my number guessing program
-print('This is a Guessing Game')
+
+#this is my number guessing program
+
+print("This is a Guessing Game")
 print("Ready to play?")
 
 
@@ -7,13 +9,13 @@ print("Ready to play?")
 #there are two users who are competing
 number_2_guess = 7
 print(number_2_guess)
-user_guess= 5
-user2_guess = 7
-print(type(user_guess))
-print(user_guess)
-if number_2_guess == user_guess:
+user_guess= input("User #1 guess: ")
+user2_guess = input("User #2 guess: ")
+
+ 
+if number_2_guess == int(user_guess):
     print("Good guess guest1, you are a winner")
-elif number_2_guess == user2_guess:
+elif number_2_guess == int(user2_guess):
     print("Good guess guest2, you are a winner!")
 else:
-    print(str(user_guess) + " is not equal to " + str(number_2_guess) + ". Better luck next time!")
+    print(str(user_guess) + " or " + str(user2_guess) + " are not equal to " + str(number_2_guess) + ". Better luck next time!")
