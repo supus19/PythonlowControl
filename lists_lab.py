b19f37ea7b6290a6b738664fa51a89925c13a167
supus19 <saaketh2127@gmail.com>
2025-01-23 07:59:37 -0800

    list_preview

---
+++ lists_lab.py
@@ -0,0 +1,15 @@
+thislist = ["apple", "banana", "cherry"]
+print(thislist)
+
+print(len(thislist))
+
+list1 = ["apple", "banana", "cherry"]
+list2 = [1, 5, 7, 9, 3]
+list3 = [True, False, False]
+
+mylist = ["apple", "banana", "cherry"]
+print(type(mylist))
+
+
+thislist = list(("apple", "banana", "cherry")) 
+print(thislist)
