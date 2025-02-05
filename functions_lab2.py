2dcbc5f9fa4610847cc87879fd7ff79712358b69
supus19 <saaketh2127@gmail.com>
2025-02-05 08:19:42 -0800

    functions lab 2

---
+++ functions_lab2.py
@@ -0,0 +1,25 @@
+# Functions Introduction
+# Python Lab - Functions - min() max()
+print('Playing with Builtin Python Functions')
+print('min() max()')
+student_name = "Saaketh Shanbhogue"
+program_creation_date = "02/05/2025"
+print(student_name)
+print(program_creation_date)
+print('''Functions Introduction
+There is a big list of built-in functions here:
+https://www.w3schools.com/python/python_ref_functions.asp
+''') 
+print('''This Lab is about using Python Builtin Functions min() and max().
+Try and show code using the following functions:
+- max()
+- min()
+
+My code follows:''')
+
+my_string = input("Enter a long string: ")
+print('The variable "my_string" = ',my_string)
+biggest = max(my_string)
+print('The biggest character in the \nstring "STEAM Is Cool" is',biggest)
+smallest = min(my_string)
+print('The smallest character in the \nstring "STEAM Is Cool" is',smallest)
