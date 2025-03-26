f2347e73b05d56e66eac5a4ced6dea3900d96473
supus19 <55859540+supus19@users.noreply.github.com>
2025-03-26 09:05:23 -0700

    Update makey_bot.py

--- makey_bot.py
+++ makey_bot.py
@@ -22,7 +22,7 @@ red_led = LED(3)
 yellow_led = LED(4)
 green_led = LED(17)
 
-myGPIO = 21
+myGPIO = 22
 
 myCorrection = 0.45
 maxPW = (2.0+myCorrection)/1000
@@ -31,11 +31,12 @@ minPW = (1.0+myCorrection)/1000
 servo = Servo(myGPIO, min_pulse_width = minPW, max_pulse_width = maxPW)
 
 def wave():
-    wave_delay = 0.5
-    servo.max()
-    sleep(wave_delay)
-    servo.min()
-    sleep(wave_delay)
+    while True:
+        wave_delay = 1
+        servo.max()
+        sleep(wave_delay)
+        servo.min()
+        sleep(wave_delay)
 
 def test_hardware():
     red_pwm_pin.value = 0.5
@@ -89,18 +90,18 @@ def stop_light(traffic_light):
         print("YELLOW LED On")
         yellow_led.on()
         time.sleep(1)
-        yellow_led.off()
-        yellow = 0
-        time.sleep(1)
-        print("YELLOW LED Off")
+#         yellow_led.off()
+#         yellow = 0
+#         time.sleep(1)
+#         print("YELLOW LED Off")
     if red != 1 and yellow !=1 and green == 1:
         print("GREEN LED On")
         green_led.on()
         time.sleep(1)
-        green_led.off()
-        green = 0
-        time.sleep(1)
-        print("GREEN LED Off")
+#         green_led.off()
+#         green = 0
+#         time.sleep(1)
+#         print("GREEN LED Off")
 
 def eyes_RGB(eyes):
     red, green, blue = eyes
@@ -125,10 +126,15 @@ def get_robot_feature_data():
         for i in color:
             if i not in hex:
                 raise Exception("Not a valid code")
-    red_rgb = int[color[0:2],16]/255
-    green_rgb = int[color[2,4], 16]/255
-    blue_rgb = int[color[4:],16]/255
-    
+           
+           
+    r = color[0:2]
+    g = color[2:4]
+    b = color[4:]
+    red_rgb = int(r,16)/255
+    green_rgb = int(g, 16)/255
+    blue_rgb = int(b,16)/255
+   
     eye = {'red_rgb_led': red_rgb, 'green_rgb_color':green_rgb, 'blue_rgb_color':blue_rgb}
     # servo
     rfd = [stop_light, eye]
@@ -137,7 +143,7 @@ def get_robot_feature_data():
 
 def main():
     print("Welcome To The STEAM Clown Makey Bot")
-    test_hardware()
+#     test_hardware()
 #    robot_features = get_robot_feature_data()
     stop_light_LEDs, eyes_rgb = get_robot_feature_data()
 
